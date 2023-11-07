e0e2de0cffa72af75721aeceab725f11d6dc3ed7
Nquartey17 <niikwarteiq@gmail.com>
2023-11-07 10:40:00 -0500

    Adding DeleteNode

---
+++ src/DeleteNode.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+    }
+}
+
+public class DeleteNode {
+    public static void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode node4 = new ListNode(9);
+        ListNode node3 = new ListNode(1);
+        ListNode node2 = new ListNode(5);
+        ListNode node1 = new ListNode(4);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = null;
+        deleteNode(node2);
+    }
+}
+
