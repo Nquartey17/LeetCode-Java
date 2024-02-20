b92d73517be946f809e750471ec179cc33372b68
Nquartey17 <niikwarteiq@gmail.com>
2024-02-20 10:32:28 -0500

    Characters don't need to be in order, I need another way to solve this problem

--- src/LengthOfLongestSubstring.java
+++ src/LengthOfLongestSubstring.java
@@ -1,24 +1,25 @@
 import java.util.HashSet;
 
+//Characters don't have to be in order
 public class LengthOfLongestSubstring {
     public static int lengthOfLongestSubstring(String s) {
-        String [] array = s.split("");
         int maxLength = 0;
-        HashSet<String> set = new HashSet<>();
+        HashSet<Character> set = new HashSet<>();
         for (int i = 0; i < s.length(); i++) {
-            if (!set.contains(array[i])) {
-                set.add(array[i]);
+            if (!set.contains(s.charAt(i))) {
+                set.add(s.charAt(i));
             } else {
                 maxLength = Math.max(maxLength, set.size());
                 set.clear();
-                set.add(array[i]);
+                set.add(s.charAt(i));
             }
         }
-        
+        //last check in case all characters are unique
+        maxLength = Math.max(maxLength, set.size());
         return maxLength;
     }
 
     public static void main(String[] args) {
-        System.out.println(lengthOfLongestSubstring("pwwwkew"));
+        System.out.println(lengthOfLongestSubstring("dvdf"));
     }
 }
