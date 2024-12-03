53203aa3bad39555dbe66b5a6840be836f4aa1a3
Nquartey17 <niikwarteiq@gmail.com>
2024-12-03 09:53:54 -0500

    Simplified solution, still not 100%

--- src/StringIsPrefixOfArray.java
+++ src/StringIsPrefixOfArray.java
@@ -2,22 +2,15 @@ public class StringIsPrefixOfArray {
     public static boolean isPrefixString(String s, String[] words) {
         //remember this is a prefix so the first word in words has to match string
         StringBuilder strBuild = new StringBuilder();
-        //check first word in string
-        if (!s.startsWith(words[0])) {
-            return false;
-        }
-        strBuild.append(words[0]);
-        for (int i = 1; i < words.length; i++) {
-            if (s.contains(words[i])) {
-                strBuild.append(words[i]);
-                if (strBuild.toString().equals(s)) {
-                    return true;
-                }
-            } else {
-                return strBuild.toString().equals(s);
+
+        for (String word : words) {
+            strBuild.append(word);
+
+            if (s.contentEquals(strBuild)) {
+                return true;
             }
         }
-        return strBuild.toString().equals(s);
+        return false;
     }
 
     public static void main(String[] args) {
