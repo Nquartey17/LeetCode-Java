5d0d5dad22a669dc7b68d92caa8f999f6fcb7e4e
Nquartey17 <niikwarteiq@gmail.com>
2024-12-04 10:52:37 -0500

    Shortened code

--- src/IsSubsequence.java
+++ src/IsSubsequence.java
@@ -3,20 +3,16 @@ public class IsSubsequence {
     public static boolean isSubsequence(String s, String t) {
         //don't use contains because it might not be in order
         //Avoid nested for loop
-        StringBuilder strBuild = new StringBuilder();
+
         int i = 0;
         int j = 0;
         while (i < s.length() && j < t.length()) {
             if (s.charAt(i) == t.charAt(j)) {
-                strBuild.append(s.charAt(i));
                 i++;
-                j++;
-            } else {
-                j++;
             }
+            j++;
         }
-
-        return strBuild.toString().equals(s);
+        return i == s.length();
     }
 
     public static void main(String[] args) {
